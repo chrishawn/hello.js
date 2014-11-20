bbe5f1a6cc9f5ad24ac284fe82df6d1a2e460049
chrishawn <chrishawn@gmail.com>
2014-11-19 23:07:25 -0600

    added daily cred module

--- tests/specs/e2e/modules.js
+++ tests/specs/e2e/modules.js
@@ -11,7 +11,8 @@ define([
 	'../../../src/modules/linkedin',
 	'../../../src/modules/foursquare',
 	'../../../src/modules/github',
-	'../../../src/modules/soundcloud'
+	'../../../src/modules/soundcloud',
+	'../../../src/modules/dailycred',
 ], function(
 //	hello
 	error_response
